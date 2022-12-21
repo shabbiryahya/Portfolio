8c3dafb8d26ecd381b28e8f0085d988d6538a9d5
shabbir yahya <shabbirkatlariwala@gmail.com>
2022-12-21 19:30:07 +0530

    Modified:portfolio

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -15,16 +15,23 @@ function Header() {
     <header>
       <div className="container header_container">
         <h5>Hello I'm</h5>
-
-        <h1>Shabbir Yahya</h1>
-        <MdVerified className="verified" />
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <h1>Shabbir Yahya</h1>{' '}
+          <span>
+            <MdVerified className="verified" />
+          </span>
+        </div>
 
         <h5 className="text-light">Fullstack Developer</h5>
         <CTA />
         <HeaderSocials />
-        <div className="me">
-          <img src={me} alt="me" />
-        </div>
+        <div className="me"><img src={me} alt="me" /></div>
         <a href="#contact" className="scroll_down">
           Scroll Down
         </a>
