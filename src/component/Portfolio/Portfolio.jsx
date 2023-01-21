507f32306ef9741805cf95d40fb9f20fcec74060
shabbir yahya <shabbirkatlariwala@gmail.com>
2023-01-21 18:30:42 +0530

    Added:flipakart portfolio,cv,readme.md

--- src/component/Portfolio/Portfolio.jsx
+++ src/component/Portfolio/Portfolio.jsx
@@ -28,6 +28,14 @@ const portfolio = [
     github: 'https://github.com/shabbiryahya/BlissWorld-clone',
     demo: 'https://blissworld-clone-alpha.netlify.app/',
   },
+  {
+    id: 4,
+    image:
+      'https://camo.githubusercontent.com/fdae98ad115f5ac478d95e537f239b070f24bb7ff310420d6c202e9ad26651f2/68747470733a2f2f692e6962622e636f2f66464b373252472f323032332d30312d30392d312e706e67',
+    title: 'Clone | Flipkart.com| HTML, CSS, Advance JS, React Js , Redux ',
+    github: 'https://github.com/shabbiryahya/Flipkart',
+    demo: 'https://flipkart-clone-masai.netlify.app/',
+  },
 ];
 const Portfolio = () => {
   return (
